7d3a1b922679513543d304925bca5f1efa5d1d75
hariharen96 <hariharen1996@gmail.com>
2024-12-13 16:47:15 +0530

    feat: update api,spinner,edge cases

--- index.js
+++ index.js
@@ -2,64 +2,126 @@ let searchInputEl = document.getElementById("searchInput");
 let searchResultsEl = document.getElementById("searchResults");
 let spinnerEl = document.getElementById("spinner");
 
+
+const displaySpinner = (isVisible) => {
+  if(isVisible){
+    spinnerEl.classList.remove('d-none')
+  }else{
+    spinnerEl.classList.add('d-none')
+  }
+}
+
+const iconElement = (iconTextSm, iconTextLg, paraText, alertBg) => {
+  let divEl = document.createElement("div");
+  divEl.classList.add("alert", alertBg, "ml-2", "mr-2");
+
+  let iconEl = document.createElement("i");
+  iconEl.classList.add(iconTextSm, iconTextLg);
+  iconEl.style.marginRight = "10px";
+
+  let p = document.createElement("p");
+  p.classList.add("alert");
+  p.textContent = paraText;
+
+  divEl.appendChild(p);
+
+  p.prepend(iconEl);
+
+  return divEl;
+};
+
 function createAndAppendResults(searchData) {
   let { title, link, description } = searchData;
 
   let resultItemEl = document.createElement("div");
-  resultItemEl.classList.add("result-item", "shadow-sm");
-  searchResultsEl.appendChild(resultItemEl);
+  resultItemEl.classList.add("result-item", "shadow-sm", "card");
 
   let titleEl = document.createElement("a");
   titleEl.href = link;
   titleEl.target = "_blank";
-  titleEl.textContent = title;
+  titleEl.textContent = title || "No title available";
   titleEl.classList.add("result-title");
-  resultItemEl.appendChild(titleEl);
-
-  let titleBreakEl = document.createElement("br");
-  resultItemEl.appendChild(titleBreakEl);
 
   let urlEl = document.createElement("a");
   urlEl.textContent = link;
-  urlEl.href = link;
+  urlEl.href = link || "No link available";
   urlEl.classList.add("result-url");
   urlEl.target = "_blank";
-  resultItemEl.appendChild(urlEl);
-
-  let lineBreakEl = document.createElement("br");
-  resultItemEl.appendChild(lineBreakEl);
 
   let descriptionEl = document.createElement("p");
-  descriptionEl.textContent = description;
+  descriptionEl.textContent = description || "No description available";
   descriptionEl.classList.add("link-description");
-  resultItemEl.appendChild(descriptionEl);
+
+  let brEl = document.createElement("br");
+
+  resultItemEl.append(titleEl, brEl, urlEl, brEl, descriptionEl);
+  searchResultsEl.appendChild(resultItemEl);
 }
 
 function displayResults(results) {
-  spinnerEl.classList.toggle("d-none");
-  for (let searchData of results) {
-    createAndAppendResults(searchData);
+  if (results.length === 0) {
+    const element = iconElement(
+      "fa-solid",
+      "fa-face-smile",
+      "No results found! :)",
+      "alert-warning"
+    );
+
+    searchResultsEl.appendChild(element);
+  } else {
+    results.forEach((items) => createAndAppendResults(items));
   }
+  displaySpinner(false)
 }
 
-function wikipediaSearch(event) {
+async function wikipediaSearch(event) {
   if (event.key === "Enter") {
-    spinnerEl.classList.toggle("d-none");
+    displaySpinner(true)
     searchResultsEl.textContent = "";
-    let searchInputElValue = searchInputEl.value;
-    let url = "https://apis.ccbp.in/wiki-search?search=" + searchInputElValue;
-    let options = {
+    let searchInputElValue = searchInputEl.value.trim();
+
+    if (searchInputElValue === "") {
+      displaySpinner(false)
+      return;
+    }
+
+    const url = "https://apis.ccbp.in/wiki-search?search=" + searchInputElValue;
+    const options = {
       method: "GET",
     };
 
-    fetch(url, options)
-      .then(function (response) {
-        return response.json();
-      })
-      .then(function (jsonData) {
-        let { search_results } = jsonData;
+    try {
+      const response = await fetch(url, options);
+      const data = await response.json();
+
+      const { search_results } = data;
+
+      if (search_results.length !== 0) {
         displayResults(search_results);
-      });
+      }else{
+        const element = iconElement(
+          "fa-solid",
+          "fa-face-smile",
+          "No results found! :)",
+          "alert-warning"
+        );
+    
+        searchResultsEl.appendChild(element);
+        displaySpinner(false)
+      }
+    } catch (err) {
+      console.log(err.message);
+      displaySpinner(false)
+
+      const element = iconElement(
+        "fa-solid",
+        "fa-circle-exclamation",
+        err.message,
+        "alert-danger"
+      );
+
+      searchResultsEl.appendChild(element);
+    }
   }
 }
 
